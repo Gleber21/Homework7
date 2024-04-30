e0ee45774ceec7a8e6817f3ee4a2b66642eb8036
Gleber21 <163034411+Gleber21@users.noreply.github.com.>
2024-04-30 17:37:20 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 1
+
+// void FromMtoN(int m, int n)
+// {
+//     if (m > n)
+//     {
+//         FromMtoN(m - 1, n);
+//         System.Console.Write(m + " ");
+//     }
+//     else if (n > m)
+//     {
+//         FromMtoN(m, n - 1);
+//         System.Console.Write(n + " ");
+//     }
+//     else System.Console.Write(m + " ");
+// }
+// FromMtoN(7, 4);
+
+// Задача 2
+
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// AkkermanFunction(m,n);
+
+// void AkkermanFunction(int m, int n)
+// {
+//     Console.Write(Akkerman(m, n)); 
+// }
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
+
+
+void print(int [] A, int n) 
+    { 
+      if (n > 0) 
+      { 
+      	Console.Write(A[n] + " "); 
+      	print(A, n - 1); 
+      } 
+      else Console.WriteLine(A[0]); 
+    } 
+    { 
+      int [] num = {1, 2, 5, 0, 10, 34}; 
+      print(num, num.Length - 1); 
+    }  
+     
